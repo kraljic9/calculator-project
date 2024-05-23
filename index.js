214e7f5544df45ef6de88035d7ee4df11e7c1ae0
Pavle Kraljic <kraljicpavle9@gmail.com>
2024-05-23 23:26:41 +0200

    adding functions to button, to display on screen

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
+let display = document.getElementById('screen-txt');
+let numberBtn = document.querySelectorAll('.number');
+let opBtn = document.querySelectorAll('.function')
+const keys = document.querySelectorAll('.key')
+
+
 let a;
 let b;
 let operator;
 
-
-let add = (a, b) => {
+function add(a, b) {
     return a + b
 }
 
-let subtract = (a, b) => {
+function subtract(a, b) {
     return a - b
 }
 
-let multiply = (a, b) => {
+function multiply(a, b) {
     return a * b
 }
 
-let divide = (a, b) => {
+function divide(a, b) {
     return a / b
 }
 
+function operate(op, a, b) {
+    switch(op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            throw new Error("Unsupported operator")
+    }
+}
+
+keys.forEach(key => {
+    key.addEventListener('click', event => {
+        if (key.classList.contains('clear')) {
+            display.textContent = "";
+        } else {
+            display.textContent += event.target.textContent;
+        }
+    });
+});
+
+opBtn.forEach(opB => {
+    opB.addEventListener('click', event => {
+        if(opB.classList.contains('clear')) {
+            display.textContent = "";
+        } else {
+            display.textContent += event.target.textContent;
+        }
+    })
+})
