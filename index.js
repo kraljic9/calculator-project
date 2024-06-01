5d6151669e495b6ec993ee39b8b0d8b77ce15de6
Pavle Kraljic <kraljicpavle9@gmail.com>
2024-06-01 14:14:00 +0200

    finished calculator

--- index.js
+++ index.js
@@ -1,60 +1,94 @@
-let display = document.getElementById('screen-txt');
-let numberBtn = document.querySelectorAll('.number');
-let opBtn = document.querySelectorAll('.function')
-const keys = document.querySelectorAll('.key')
+let operator = "";
+let previousValue = "";
+let currentValue = "";
 
+document.addEventListener("DOMContentLoaded", function() {
+    //Store all components on HTML in our JS
+    let clear = document.querySelector('#clear-btn');
+    let equal = document.querySelector('#eaqual');
+    let decimal = document.querySelector('#decimal');
 
-let a;
-let b;
-let operator;
+    let numbers = document.querySelectorAll('.number');
+    let operators = document.querySelectorAll('#operator');
 
-function add(a, b) {
-    return a + b
-}
+    let previousScreen = document.querySelector('#previous');
+    let currentScreen = document.querySelector('#current');
 
-function subtract(a, b) {
-    return a - b
-}
+    numbers.forEach((number) => number.addEventListener("click", function(e) {
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue
+    }))
 
-function multiply(a, b) {
-    return a * b
-}
+    operators.forEach((op) => op.addEventListener('click', function(e) {
+        handleOperator(e.target.textContent)
+        previousScreen.textContent += previousValue + " " + operator;
+        currentScreen.textContent = currentValue;
+    }))
 
-function divide(a, b) {
-    return a / b
-}
+    clear.addEventListener("click", function() {
+        previousValue = '';
+        currentValue = '';
+        operator = '';
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
 
-function operate(op, a, b) {
-    switch(op) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case '*':
-            return multiply(a, b);
-        case '/':
-            return divide(a, b);
-        default:
-            throw new Error("Unsupported operator")
+    equal.addEventListener("click", function() {
+        if (currentValue =! '' && previousValue != '') { 
+        calculate()
+        previousScreen.textContent = '';
+        if(previousValue.length <= 5){
+            currentScreen.textContent = previousValue
+        } else {
+            currentScreen.textContent = previousValue.slice(0,5) + '...'
+        }
+        currentScreen.textContent = previousValue;
+        }
+    })
+
+    decimal.addEventListener("click", function() {
+        addDecimal()
+    })
+})
+
+function handleNumber(num) {
+    if (currentValue.length <= 5){
+        currentValue += num; 
     }
 }
 
-keys.forEach(key => {
-    key.addEventListener('click', event => {
-        if (key.classList.contains('clear')) {
-            display.textContent = "";
-        } else {
-            display.textContent += event.target.textContent;
-        }
-    });
-});
+function handleOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = "";
+}
 
-opBtn.forEach(opB => {
-    opB.addEventListener('click', event => {
-        if(opB.classList.contains('clear')) {
-            display.textContent = "";
-        } else {
-            display.textContent += event.target.textContent;
+function calculate() {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator === "+") {
+        previousValue += currentValue;
+    } else if (operator === "-") {
+        previousValue -= currentValue;
+    } else if (operator === "*") {
+        previousValue *= currentValue;
+    } else {    
+        previousValue /= currentValue;
+    }
+
+    previousValue = roundNumber(previousValue)
+    previousValue = previousValue.toString();
+    currentValue = currentValue.toString();
+}
+
+function roundNumber(num) {
+    return Math.round(num * 1000) / 1000
+}
+
+function addDecimal() {
+    if(!currentValue.includes('.'))
+        {
+            currentValue += '.'
         }
-    })
-})
+}
